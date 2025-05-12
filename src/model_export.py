69c81a9cc3becb180bb285bf905862987165ffb3
Carlos <clizaga@gmail.com>
2025-05-12 21:43:47 +0200

    export model

--- src/model_export.py
+++ src/model_export.py
@@ -1,10 +1,56 @@
 from pathlib import Path
+import os
 
-import onnx
+import os
 
+os.environ.pop("CUDA_VISIBLE_DEVICES", None)
+
+import torch  # tras limpiar, comprobamos disponibilidad
+if torch.cuda.is_available():
+    os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+else:
+    os.environ["CUDA_VISIBLE_DEVICES"] = ""
+
+
+from ultralytics import YOLO
+
+
+model_subdir = '../models/best.pt'
 
 BASE_DIR = Path(__file__).resolve().parent
-SAVE_DIR = BASE_DIR / 'models'
+MODEL_DIR = (BASE_DIR / model_subdir) .resolve()
 
 
-x
+
+model = YOLO(MODEL_DIR)
+
+# Formato ONNX multiplataforma
+model.export(    
+    format="onnx",
+    opset=12,            # Compatibilidad con ONNX Runtime
+    simplify=True,       # Limpia la red para hacerla más ligera
+    imgsz=1024,          # Tamaño base (se puede cambiar luego si usas entrada dinámica)
+    # nms=True,          # True para evitar la post-pro manual
+    device = 'cpu'
+    )
+
+# Formato CoreML para MacOs
+model.export(
+    format="coreml",
+    opset=12,            # Compatibilidad con ONNX Runtime
+    simplify=True,       # Limpia la red para hacerla más ligera
+    imgsz=1024,           # Tamaño base (se puede cambiar luego si usas entrada dinámica)
+    # nms=True,           # True para evitar la post-pro manual
+    device = 'cpu'
+)
+
+# Formato TensorRT para usar graficas Nvidia
+model.export(
+    format="engine",
+    imgsz=1024,
+    half=True,                  # Usa FP16 si GPU lo soporta
+    device = 'cuda:0',
+)
+
+from ultralytics.utils.benchmarks import benchmark
+benchmark(model=YOLO(MODEL_DIR / "best.onnx"))
--- src/test_model.py
+++ src/test_model.py
@@ -8,11 +8,19 @@ BASE_DIR = Path(__file__).resolve().parent
 YAML_DIR = BASE_DIR / '../data/raw/labels'
 YAML_DIR.resolve()
 
-MODEL_DIR = BASE_DIR/ '../runs/detect/train2/weights'
+MODEL_DIR = BASE_DIR/ '../models'
 MODEL_DIR.resolve()
 
 
-model = YOLO(MODEL_DIR/'best.pt')
-metrics = model.val(data=YAML_DIR/'data.yaml', split='val', conf=0.25, iou=0.7, imgsz=1024)
+model = YOLO('./runs/detect/fine_tunning/weights/last.pt')
+metrics = model.val(data=YAML_DIR/'data.yaml', split='val', imgsz=1024)
 
 print(metrics)
+
+
+
+from ultralytics.utils.benchmarks import benchmark
+benchmark(model=YOLO(MODEL_DIR / "best.pt", data = YAML_DIR))
+""" benchmark(model=YOLO(MODEL_DIR / "best.onnx", data = YAML_DIR))
+benchmark(model=YOLO(MODEL_DIR / "best.mlpackage/Data/com.apple.CoreML/model.mlmodel", data = YAML_DIR))
+benchmark(model=YOLO(MODEL_DIR / "best.engine", data = YAML_DIR)) """
